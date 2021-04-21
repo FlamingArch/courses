5103ee71ad514223e8a6966cc7b9bb23a6fef941
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-04-22 00:33:59 +0530

    Day 1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+// String
+var str = "Hello, playground"
+str="Goodbye"
+
+// Int
+var age = 38
+var population = 8_000_000_000
+
+// Making String Multiline
+var str1 = """
+This Goes
+Over
+Multiple Lines
+"""
+
+var str2 =  """
+This Goes \
+Over \
+Multiple Lines
+"""
+// Used to only put newline in code, but not actually in the string variable
+
+// Double
+var pi = 3.141592
+
+// Boolean
+var awesome = true
+
+// String Interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var str4 = "The test results are as follows: \(str3)"
+
+// Constants
+let taylor = "Swift"
+// taylor = "Shaw"      // Throws error
+
+// Type annotations
+let str5 = "Hello Playgrounds"
+// Manually set it as string
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
