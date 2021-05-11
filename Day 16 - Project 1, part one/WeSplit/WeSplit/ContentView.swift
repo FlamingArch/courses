3bd377c0a435404ed5080c3e5f7ad9fa28ab6520
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-11 22:18:32 +0530

    Day 16 - Project 1, part one

---
+++ Day 16 - Project 1, part one/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Harsh Chaturvedi on 11/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {y
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Day 16 - Project 1, part one/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Harsh Chaturvedi on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
