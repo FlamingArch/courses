9bcd350d868de892c9b4db9bd389ceca0e4020c5
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-12 21:07:55 +0530

    Day 17 - Project 1, part two

--- Day 16 - Project 1, part one/WeSplit/WeSplit/ContentView.swift
+++ Day 16 - Project 1, part one/WeSplit/WeSplit/ContentView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkOutAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10,15,20,25,0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkOutAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        
+        return amountPerPerson
+    }
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkOutAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How Much Tip You Want to Leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
