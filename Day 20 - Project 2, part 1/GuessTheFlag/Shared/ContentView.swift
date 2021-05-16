c605c0b8cb15955cb66d541e79a5e0f3145d33dd
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-16 17:42:24 +0530

    Day 21 - Project 2, part 2

--- Day 20 - Project 2, part 1/GuessTheFlag/Shared/ContentView.swift
+++ Day 20 - Project 2, part 1/GuessTheFlag/Shared/ContentView.swift
@@ -8,44 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    //    @State private var showingAlert = false
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     var body: some View {
-        //        NavigationView {
-        //            VStack {
-        //                HStack {
-        //                    Rectangle().foregroundColor(.red)
-        //                    Rectangle().foregroundColor(.pink)
-        //                    Rectangle().foregroundColor(.purple)
-        //                }
-        //                HStack {
-        //                    Rectangle().foregroundColor(.blue)
-        //                    Rectangle().foregroundColor(.green)
-        //                    Rectangle().foregroundColor(.yellow)
-        //                }
-        //                HStack {
-        //                    Rectangle().foregroundColor(.orange)
-        //                    Rectangle().foregroundColor(.gray)
-        //                    Rectangle().foregroundColor(.black)
-        //                }
-        //            }
-        //            .navigationTitle("Sample")
-        //        }
-        //        ZStack {
-        //            Color(red: 1, green: 0.8, blue: 0)
-        //                .edgesIgnoringSafeArea(.all)
-        //                .frame(width: 200, height: 200, alignment: .center)
-        //            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-        //            AngularGradient(gradient: Gradient(colors: [Color.red, .yellow, .green,  Color.blue, .purple, .pink]), center: .center)
-        //            Text("Your Content")
-        //        }
-        Button(action: {
-            self.showingAlert = true
-        }, label: {
-            Image(systemName: "pencil")
-            Text("Edit")
-        }).alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI"), message: Text("This is a detailed message"), dismissButton: .default(Text("OK")))
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer]).font(.largeTitle).fontWeight(.black)
+                }.foregroundColor(.white)
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        // Flag was tapped
+                        self.flagTapped(number)
+                    }, label: {
+                        Image(self.countries[number]).clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    })
+                }
+                Spacer()
+            }
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your Score is ???"), dismissButton: .default(Text("Continue"))  {
+                self.askQuestion()
+            })
         }
+    }
+    func flagTapped(_ number: Int) {
+        scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -54,3 +56,45 @@ struct ContentView: View {
 //         ContentView()
 //     }
 // }
+
+
+//    @State private var showingAlert = false
+
+//var body: some View {
+//    NavigationView {
+//        VStack {
+//            HStack {
+//                Rectangle().foregroundColor(.red)
+//                Rectangle().foregroundColor(.pink)
+//                Rectangle().foregroundColor(.purple)
+//            }
+//            HStack {
+//                Rectangle().foregroundColor(.blue)
+//                Rectangle().foregroundColor(.green)
+//                Rectangle().foregroundColor(.yellow)
+//            }
+//            HStack {
+//                Rectangle().foregroundColor(.orange)
+//                Rectangle().foregroundColor(.gray)
+//                Rectangle().foregroundColor(.black)
+//            }
+//        }
+//        .navigationTitle("Sample")
+//    }
+//    ZStack {
+//        Color(red: 1, green: 0.8, blue: 0)
+//            .edgesIgnoringSafeArea(.all)
+//            .frame(width: 200, height: 200, alignment: .center)
+//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+//        AngularGradient(gradient: Gradient(colors: [Color.red, .yellow, .green,  Color.blue, .purple, .pink]), center: .center)
+//        Text("Your Content")
+//    }
+//    Button(action: {
+//        self.showingAlert = true
+//    }, label: {
+//        Image(systemName: "pencil")
+//        Text("Edit")
+//    }).alert(isPresented: $showingAlert) {
+//        Alert(title: Text("Hello SwiftUI"), message: Text("This is a detailed message"), dismissButton: .default(Text("OK")))
+//    }
+//}
