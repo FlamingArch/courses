60748481c68114c73137c1f136f02cda54cbb3c6
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-06-13 23:02:36 +0530

    Day 37 - Project 7, part 2 PARTIALLY

--- Day 36 - Project 7, part 1/iExpense/Shared/ContentView.swift
+++ Day 36 - Project 7, part 1/iExpense/Shared/ContentView.swift
@@ -7,9 +7,65 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+                
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Sup")
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar{
+                Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
