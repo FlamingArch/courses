c69ffe9b90115339d8d9af9cba8094a05048f484
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-06-16 13:47:27 +0530

    Day 39 – Project 8, part one

---
+++ Day 39 - Project 8, part 1/Moonshot/Shared/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 16/06/21.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+                {
+                    "name": "Taylor Swift",
+                    "address": {
+                        "street": "555, Taylor Swift Avenue",
+                        "city": "Nashville"
+                    }
+                }
+                """
+            struct User: Codable {
+                var name: String
+                var address: Address
+            }
+
+            struct Address: Codable {
+                var street: String
+                var city: String
+            }
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+//struct ContentView: View {
+//    var body: some View {
+//        NavigationView {
+//            List(0..<100) { row in
+//                NavigationLink ( destination: Text("Detail \(row)") ) { Text("Row \(row)") }
+//            }
+//            .navigationTitle("SwiftUI")
+//        }
+//    }
+//}
+
+//struct CustomText: View {
+//    var text: String
+//    var body: some View {
+//        Text(text)
+//    }
+//    init(_ text: String) {
+//        print("Creating a CustomText")
+//        self.text = text
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        ScrollView(.vertical) {
+//            VStack {
+//                ForEach(0..<100) {
+//                    CustomText("Item \($0)")
+//                        .font(.title)
+//                }
+//            }.frame(maxWidth: .infinity)
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            GeometryReader { geo in
+//                Image("bg")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fill)
+//                    .frame(width: geo.size.width)
+//                    .edgesIgnoringSafeArea(.all)
+//            }
+////                .clipped()
+//        }
+//    }
+//}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Day 39 - Project 8, part 1/Moonshot/Shared/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
