a036516da4fe919b14e70ccde53023066f53292d
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-06-26 21:45:31 +0530

    Day 43 - Project 9, part 1

---
+++ Day 43 - Project 9, part 1/Drawing/Shared/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 26/06/21.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
+    }
+}
+
+
+//struct ContentView: View {
+//    var body: some View {
+//        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+//            .stroke(Color.blue, lineWidth: 10)
+//            .frame(width: 300, height: 300)
+//    }
+//}
+
+
+//struct ContentView: View {
+//    var body: some View {
+//        Triangle()
+//            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
+//    }
+//}
+
+
+//struct ContentView: View {
+//    var body: some View {
+//        Path { path in
+//            path.move(to: CGPoint(x:200, y: 100))
+//            path.addLine(to: CGPoint(x: 100, y: 300))
+//            path.addLine(to: CGPoint(x: 300, y: 300))
+//            path.addLine(to: CGPoint(x: 200, y: 100))
+//
+//        }
+//        .stroke(Color.blue.opacity(0.10),style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//
+//    }
+//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 43 - Project 9, part 1/Drawing/Shared/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
