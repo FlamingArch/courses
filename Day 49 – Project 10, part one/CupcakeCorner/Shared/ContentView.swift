a261e839546644b8d60e9b220a1c292dd8c0aa74
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-08-18 18:10:14 +0530

    Day 49 – Project 10, part one

---
+++ Day 49 – Project 10, part one/CupcakeCorner/Shared/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 18/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var username = ""
+    @State var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            Section {
+                Button("Create Account") {
+                    print("Creating Account")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+}
+
+//struct Response: Codable {
+//    var results: [Result]
+//}
+//
+//struct Result: Codable {
+//    var trackId: Int
+//    var trackName: String
+//    var collectionName: String
+//}
+//
+//struct ContentView: View {
+//    @State var results = [Result]()
+//    var body: some View {
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//            }
+//        }.onAppear(perform: loadData)
+//    }
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        let request = URLRequest(url: url)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        self.results = decodedResponse.results
+//                    }
+//                    return
+//                }
+//            }
+//            print("Fetch Failed: \(error?.localizedDescription ?? "Unknown Error")")
+//        }.resume()
+//    }
+//}
+
+//class User: ObservableObject, Codable {
+//    enum CodingKeys: CodingKey {
+//        case name
+//    }
+//
+//    required init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        name = try container.decode(String.self, forKey: .name)
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(name, forKey: .name)
+//    }
+//
+//    @Published var name = "Paul Hudson"
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, world!")
+//            .padding()
+//    }
+//}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Day 49 – Project 10, part one/CupcakeCorner/Shared/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
