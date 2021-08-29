b009031d6bee863b4da2e175c88d52a68d3a5e49
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-08-29 15:33:50 +0530

    Day 54 – Project 11, part two

--- Day 53 – Project 11, part one/Bookworm/Shared/ContentView.swift
+++ Day 53 – Project 11, part one/Bookworm/Shared/ContentView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Sup")
+        NavigationView {
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                        EmojiRatingView(rating: book.rating)
+                        VStack(alignment:.leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                            
+                        }
+                    }
+                }
+            }
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .toolbar {
+                    Button { self.showingAddScreen.toggle() } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                        .environment(\.managedObjectContext, self.moc)
+                }
+        }
     }
 }
 
