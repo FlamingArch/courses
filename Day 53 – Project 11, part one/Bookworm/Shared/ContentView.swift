ead75c9040814e95b063a86f0397cc701a5ac784
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-09-05 20:14:14 +0530

    Day 55 – Project 11, part three

--- Day 53 – Project 11, part one/Bookworm/Shared/ContentView.swift
+++ Day 53 – Project 11, part one/Bookworm/Shared/ContentView.swift
@@ -9,27 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true)
+    ]) var books: FetchedResults<Book>
     @State private var showingAddScreen = false
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(books, id: \.self) { book in
-                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                    NavigationLink(destination: DetailView(book: book)) {
                         EmojiRatingView(rating: book.rating)
                         VStack(alignment:.leading) {
                             Text(book.title ?? "Unknown Title")
                                 .font(.headline)
                             Text(book.author ?? "Unknown Author")
                                 .foregroundColor(.secondary)
-                            
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             Text("Count: \(books.count)")
                 .navigationBarTitle("Bookworm")
+                .navigationBarItems(leading: EditButton())
                 .toolbar {
                     Button { self.showingAddScreen.toggle() } label: {
                         Image(systemName: "plus")
@@ -40,6 +44,14 @@ struct ContentView: View {
                         .environment(\.managedObjectContext, self.moc)
                 }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        try? moc.save()
     }
 }
 
