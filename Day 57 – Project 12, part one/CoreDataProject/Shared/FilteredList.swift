e3f5fc0ad13e395ec31fb242a1ec4b0f2efd0013
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-10-05 00:37:23 +0530

    Day 59 – Project 12, part three

--- Day 57 – Project 12, part one/CoreDataProject/Shared/FilteredList.swift
+++ Day 57 – Project 12, part one/CoreDataProject/Shared/FilteredList.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 import CoreData
 
+enum Predicate: String {
+    case equals = "=="
+    case lessThanEquals = "<="
+    case greaterThanEquals = ">="
+    case inside = "in"
+    case beginsWith = "BEGINSWITH", beginsWithCaseInsensitive = "BEGINSWITH[c]"
+    case contains = "CONTAINS"
+    case notEquals = "NOT =="
+    case notLessThanEquals = "NOT <="
+    case notGreaterThanEquals = "NOT >="
+    case notInside = "NOT in"
+    case notBeginsWith = "NOT BEGINSWITH", notBeginsWithCaseInsensitive = "NOTBEGINSWITH[c]"
+    case notContains = "NOT CONTAINS"
+}
+
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     var fetchRequest: FetchRequest<T>
+    var sortDescripters: [NSSortDescriptor]?
     var singers: FetchedResults<T> {
         fetchRequest.wrappedValue
     }
@@ -21,8 +37,8 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        fetchRequest =  FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%k BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String, filterPredicate: Predicate = .beginsWith, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest =  FetchRequest<T>(entity: T.entity(), sortDescriptors: sortDescripters ?? [], predicate: NSPredicate(format: "%k \(filterPredicate.rawValue) %@", filterKey, filterValue))
         self.content = content
     }
 }
