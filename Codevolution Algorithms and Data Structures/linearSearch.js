3fe976ec56b288a86cb70998d21071197bc38771
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-09 13:37:27 +0530

    Codevolution DSA: Search Algorithms Completed

---
+++ Codevolution Algorithms and Data Structures/linearSearch.js
@@ -0,0 +1,10 @@
+function linearSearch(arr, k) {
+  for (let i = 0; i < arr.length; i++) {
+    if (k == arr[i]) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // 2
+console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // 4
+console.log(linearSearch([-5, 2, 10, 4, 6], 20)); // -1
