2399e69aaeeb6dee14964e7cec595f6d4f566539
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-08-27 01:39:57 -0700

    Codevolution React Native Layouts

--- Codevolution React Native/RNLayout/App.js
+++ Codevolution React Native/RNLayout/App.js
@@ -5,23 +5,10 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box
-        style={{
-          backgroundColor: "#8e9b00",
-          alignSelf: "flex-start",
-        }}
-      >
-        Box 1 {/*flex: 1*/}
-      </Box>
-      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>
-        Box 2 {/*flex: 3*/}
-      </Box>
-      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>
-        Box 3
-      </Box>
-      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
-        Box 4
-      </Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#b95f21" }}>Box 7 </Box>
@@ -31,12 +18,35 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     // flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
+    // justifyContent: "space-evenly",
+    // alignItems: "flex-start",
+    // flexWrap: "wrap",
+    // alignContent: "space-around",
+    // rowGap: 20,
+    // columnGap: 40,
+    // gap: 40,
+    // height: 300,
+    // width: 300,
+    flex: 1,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
   },
 });
+
+/*flex: 1; alignSelf: "center"*/
+/*flex: 3; alignSelf: "stretch"*/
+
+// flexBasis: 140, flex: 1
+// height: 140
+
+// flexShrink: 1
+// flexShrink: 2
+// flexGrow: 1
+// flexGrow: 3
+
+// top: 75, left: 75
+// top: 95,
+// left: 95,
+// position: "absolute",
--- Codevolution React Native/RNLayout/components/Box.js
+++ Codevolution React Native/RNLayout/components/Box.js
@@ -12,8 +12,11 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "red",
     padding: 20,
+    width: 100,
+    height: 100,
   },
   text: {
+    flexGrow: 1,
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
