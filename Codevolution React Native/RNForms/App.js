7e20d38208d4b940fd4949e47f79d12f34bb7218
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-09-13 12:19:49 +0530

    Codevolution React Native - TextInput +options, multiline

---
+++ Codevolution React Native/RNForms/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
+
+export default function App() {
+  const [name, setName] = useState("");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>My name is {name}</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        secureTextEntry={false} // For Passwords
+        keyboardType="default"
+        autoCorrect={false} // For usernames, etc
+        autoCapitalize="none" // For usernames, etc
+      />
+      <TextInput
+        style={[styles.input, styles.multilineText]}
+        placeholder="Password"
+        multiline
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: "#ccc",
+  },
+  text: {
+    fontSize: 17,
+    padding: 10,
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top", // For Android, as multiline text is not vertically aligned by default, and instead starts from the centre
+  },
+});
