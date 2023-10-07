2998475b8f620626c70d78303e49d1610fc995d4
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-10-07 18:14:28 +0530

    100 Days of SwiftUI - Optionals

---
+++ Hacking with Swift - 100 Days of SwiftUI/Optionals.playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+let opposites = ["Mario": "Wario", "Luigi": "Waluigi"]
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")   // Real string, not an optional. Will only run if value is present.
+} else {
+    print("Opposite Not Found")
+}
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+
+if let number = number {
+    print(square(number: number))
+}
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    
+    print("\(number) x \(number) is \(number * number)")
+}
+
+let peachOpposite2 = opposites["Peach"] ?? "No opposite found"
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
