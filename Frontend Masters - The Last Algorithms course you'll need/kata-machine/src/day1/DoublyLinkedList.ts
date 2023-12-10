82a462ffbc168d7e1aaea0c9d25989f3a3f93b17
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-12-10 13:59:46 +0530

    DoublyLinkedList get

--- Frontend Masters - The Last Algorithms course you'll need/kata-machine/src/day1/DoublyLinkedList.ts
+++ Frontend Masters - The Last Algorithms course you'll need/kata-machine/src/day1/DoublyLinkedList.ts
@@ -1,27 +1,29 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
     get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+        let current = this.head;
+        for (let i = 0; i < idx && current; i++) {
+            current = current.next;
+        }
+        return current?.value;
+    }
+    removeAt(idx: number): T | undefined {}
 }
