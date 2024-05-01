4df46763ab3b5f3b8e9f93463b9b18e25037053e
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-05-01 14:35:09 +0530

    Create WelcomeMessage.java

---
+++ freeCodeCamp Web Apps in Java Spring Boot 3/runnerz/src/main/java/foo/bar/WelcomeMessage.java
@@ -0,0 +1,10 @@
+package foo.bar;
+
+import com.flamingarch.runnerz.RunnerzApplication;
+import org.springframework.boot.SpringApplication;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerzApplication.class, args);
+    }
+}
