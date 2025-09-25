debe7cb676f2bede3c2c5ea35c2e2d1c0fef5d6c
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-09-26 03:36:50 +0530

    Maximilian Schwarz Angular

--- Maximilian Schwarzmuller - Angular/old/old/my-first-app/src/app/app.component.spec.ts
+++ Maximilian Schwarzmuller - Angular/old/old/my-first-app/src/app/app.component.spec.ts
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-app');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('my-first-app app is running!');
   });
 });
